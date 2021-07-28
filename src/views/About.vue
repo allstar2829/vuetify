1ef504d332d8a6f620c06d0f569167fa143759be
YI-SPACES\iris.chen <iris.chen@yi-spaces.com>
2021-07-29 01:05:26 +0800

    vuetify-todoList

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about pa-6" >
+    <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci unde odit illo magnam dolore officia aspernatur laboriosam sit enim aliquid? Nesciunt quod cumque rem voluptatibus ducimus consequuntur modi veniam cupiditate!
+    Architecto incidunt et ducimus blanditiis temporibus natus enim esse hic quaerat voluptatibus! Architecto ex labore fugit, iure, eveniet corrupti autem distinctio temporibus aliquam quidem laudantium tenetur quo deserunt praesentium omnis!
+    Aperiam minima aut praesentium molestias vitae itaque debitis magnam, sit ipsum sed. Ex ipsam nam doloribus, similique, beatae itaque, quaerat fugiat repellat voluptatum nostrum vero facilis inventore commodi veniam accusantium!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci unde odit illo magnam dolore officia aspernatur laboriosam sit enim aliquid? Nesciunt quod cumque rem voluptatibus ducimus consequuntur modi veniam cupiditate!
+    Architecto incidunt et ducimus blanditiis temporibus natus enim esse hic quaerat voluptatibus! Architecto ex labore fugit, iure, eveniet corrupti autem distinctio temporibus aliquam quidem laudantium tenetur quo deserunt praesentium omnis!
+    Aperiam minima aut praesentium molestias vitae itaque debitis magnam, sit ipsum sed. Ex ipsam nam doloribus, similique, beatae itaque, quaerat fugiat repellat voluptatum nostrum vero facilis inventore commodi veniam accusantium!</p>
+  </div>
+</template>
